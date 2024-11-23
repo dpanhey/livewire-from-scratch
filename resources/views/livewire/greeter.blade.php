ec1e7c051510f4de120565299f006c2053f3b296
Dustin Panhey <d.panhey@gmx.de>
2024-11-23 10:24:53 +0100

    Episode 2 - using actions

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,3 +1,26 @@
 <div>
-     {{ $name }} doesn't talk, he acts.
+    <div>
+        {{ $name }} doesn't talk, he acts.
+    </div>
+
+    <form
+     wire:submit="changeName(document.querySelector('#newName').value)"
+    >
+        <div class="mt-2">
+            <input
+                id="newName"
+                type="text"
+                class="block w-full p-4 border rounded-md bg-gray-700 text-white"
+            >
+
+            <div class="mt-2">
+                <button
+                    type="submit"
+                    class="text-white font-medium py-2 px-4 rounded-lg bg-blue-500 hover:bg-blue-600"
+                >
+                    Greet
+                </button>
+            </div>
+        </div>
+    </form>
 </div>
